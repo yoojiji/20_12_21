212386c5da9c2c93cbe010b76ac216a9bd7f3417
yoojiji <62611208+yoojiji@users.noreply.github.com>
2020-12-21 16:26:09 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,281 @@
+$(document).ready(function () { console.log("jquery complete") })
+
+
+$(document).ready(function () {
+    $('.demo').click(function () {
+        $('.demo').width('100%');
+    });
+});
+
+$(document).ready(function () {
+    $('.demo2').click(function () {
+        $('.demo2').width('100%');
+    });
+});
+
+$(document).ready(function () {
+    $('.demo3').click(function () {
+        $('.demo3').width('100%');
+    });
+});
+
+$(document).ready(function () {
+    $('.demo4').click(function () {
+        $('.demo4').width('100%');
+    });
+});
+
+$(document).ready(function () {
+    $('.demo5').click(function () {
+        $('.demo5').width('100%');
+    });
+});
+
+$(document).ready(function () {
+    $('.demo6').click(function () {
+        $('.demo6').width('100%');
+    });
+});
+
+$(document).ready(function () {
+    $('.demo7').click(function () {
+        $('.demo7').width('100%');
+    });
+});
+
+$(document).ready(function () {
+    $('.demo8').click(function () {
+        $('.demo8').width('100%');
+    });
+});
+
+
+
+
+
+
+
+$(document).ready(function () {
+    $('#demo').click(function () {
+        $('#demo').append('<embed id="embed_player" src="audio/사운드 1.wav" autostart="true" hidden="true"></embed>');
+    });
+});
+
+
+$(document).ready(function () {
+    $('#demo2').click(function () {
+        $('#demo2').append('<embed id="embed_player" src="audio/사운드 2.wav" autostart="true" hidden="true"></embed>');
+    });
+});
+
+$(document).ready(function () {
+    $('#demo3').click(function () {
+        $('#demo3').append('<embed id="embed_player" src="audio/사운드 3.wav" autostart="true" hidden="true"></embed>');
+    });
+});
+
+$(document).ready(function () {
+    $('#demo4').click(function () {
+        $('#demo4').append('<embed id="embed_player" src="audio/사운드 4.wav" autostart="true" hidden="true"></embed>');
+    });
+});
+
+$(document).ready(function () {
+    $('#demo5').click(function () {
+        $('#demo5').append('<embed id="embed_player" src="audio/사운드 5.wav" autostart="true" hidden="true"></embed>');
+    });
+});
+
+$(document).ready(function () {
+    $('#demo6').click(function () {
+        $('#demo6').append('<embed id="embed_player" src="audio/사운드 6.wav" autostart="true" hidden="true"></embed>');
+    });
+});
+
+$(document).ready(function () {
+    $('#demo7').click(function () {
+        $('#demo7').append('<embed id="embed_player" src="audio/사운드 7.wav" autostart="true" hidden="true"></embed>');
+    });
+});
+
+$(document).ready(function () {
+    $('#demo8').click(function () {
+        $('#demo8').append('<embed id="embed_player" src="audio/사운드 8.wav" autostart="true" hidden="true"></embed>');
+    });
+});
+
+
+
+$(document).ready(function () {
+    $('.demo').mouseover(function () {
+        $('.all').css("backgroundColor", "black");
+    });
+});
+
+
+$(document).ready(function () {
+    $('.demo').click(function () {
+        $('.all').css("backgroundColor", "black");
+    });
+});
+
+$(document).ready(function () {
+    $('.demo2').mouseover(function () {
+        $('.all').css("backgroundColor", "black");
+    });
+});
+
+
+$(document).ready(function () {
+    $('.demo3').mouseover(function () {
+        $('.all').css("backgroundColor", "black");
+    });
+});
+
+
+
+$(document).ready(function () {
+    $('.demo4').mouseover(function () {
+        $('.all').css("backgroundColor", "black");
+    });
+});
+
+
+
+$(document).ready(function () {
+    $('.demo5').mouseover(function () {
+        $('.all').css("backgroundColor", "black");
+    });
+});
+
+
+
+$(document).ready(function () {
+    $('.demo6').mouseover(function () {
+        $('.all').css("backgroundColor", "black");
+    });
+});
+
+
+
+$(document).ready(function () {
+    $('.demo7').mouseover(function () {
+        $('.all').css("backgroundColor", "black");
+    });
+});
+
+
+
+$(document).ready(function () {
+    $('.demo8').mouseover(function () {
+        $('.all').css("backgroundColor", "black");
+    });
+});
+
+
+
+
+$(document).ready(function () {
+    $('.demo').click(function () {
+        $('.이착').css("color", "black");
+    });
+});
+
+$(document).ready(function () {
+    $('.demo2').click(function () {
+        $('.이착').css("color", "black");
+    });
+});
+
+$(document).ready(function () {
+    $('.demo3').click(function () {
+        $('.이착').css("color", "black");
+    });
+});
+
+$(document).ready(function () {
+    $('.demo4').click(function () {
+        $('.이착').css("color", "black");
+    });
+});
+
+$(document).ready(function () {
+    $('.demo5').click(function () {
+        $('.이착').css("color", "black");
+    });
+});
+
+$(document).ready(function () {
+    $('.demo6').click(function () {
+        $('.이착').css("color", "black");
+    });
+});
+
+$(document).ready(function () {
+    $('.demo7').click(function () {
+        $('.이착').css("color", "black");
+    });
+});
+
+$(document).ready(function () {
+    $('.demo8').click(function () {
+        $('.이착').css("color", "black");
+    });
+});
+
+
+
+$(document).ready(function () {
+    $('.demo').click(function () {
+        $('.사클').css("color", "black");
+    });
+});
+
+$(document).ready(function () {
+    $('.demo2').click(function () {
+        $('.사클').css("color", "black");
+    });
+});
+
+$(document).ready(function () {
+    $('.demo3').click(function () {
+        $('.사클').css("color", "black");
+    });
+});
+
+$(document).ready(function () {
+    $('.demo4').click(function () {
+        $('.사클').css("color", "black");
+    });
+});
+
+$(document).ready(function () {
+    $('.demo5').click(function () {
+        $('.사클').css("color", "black");
+    });
+});
+
+$(document).ready(function () {
+    $('.demo6').click(function () {
+        $('.사클').css("color", "black");
+    });
+});
+
+$(document).ready(function () {
+    $('.demo7').click(function () {
+        $('.사클').css("color", "black");
+    });
+});
+
+$(document).ready(function () {
+    $('.demo8').click(function () {
+        $('.사클').css("color", "black");
+    });
+});
+
+
+
+
+
+
+
